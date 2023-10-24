eec5ae87e50426f0993806c21cadc61fcc4be335
QaisKhaleefa <qaisomer29@gmail.com>
2023-10-24 00:08:19 -0700

    insert and wum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data
+ * @head: point to the head of the list
+ * Return: sum of all the data
+ *         Otherwise - 0 if  list is null
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
